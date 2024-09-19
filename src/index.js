60715918af4de81167c523e9e9ff12dfe61f3459
Madhavan0803 <Madhavanraj123@gmail.com>
2024-09-19 20:47:13 +0530

    uploaded all files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import "./style.css"
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+function RandomNumber() {
+  var [randomNumber, setRandomNumber] = useState(0)
+
+  function generateRandomNumber(){
+    var random = Math.floor(Math.random() * 10)
+    setRandomNumber(random)
+  };
+
+  return (
+    <div className="container">
+      <h1>Random Number Generator</h1>
+      <div className="random-number">{randomNumber}</div>
+      <button className="generate-btn" onClick={generateRandomNumber}>
+        Generate Random Number
+      </button>
+    </div>
+  );
+}
+
+
+root.render(<RandomNumber/>)
